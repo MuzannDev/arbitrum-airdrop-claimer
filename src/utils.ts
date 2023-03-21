b11238325d451e1a29726ea483dc24dafa0ff7c1
Muz <muz@Muzs-MacBook-Pro.local>
2023-03-21 18:02:07 +0100

    add debug log

--- src/utils.ts
+++ src/utils.ts
@@ -9,7 +9,7 @@ import secrets from '../secrets.json'
 
 dotenv.config()
 
-export const l2Client = new Web3(process.env.L2_RPC_URL ? process.env.L2_RPC_URL : process.exit(1))
+export const l2Client = new Web3(process.env.L2_RPC_URL ? process.env.L2_RPC_URL : handleError('Missing L2 RPC URL'))
 const blockContract = new l2Client.eth.Contract(BLOCK_ABI as AbiItem[], process.env.BLOCK_CONTRACT_ADDRESS)
 export const distributor = new l2Client.eth.Contract(DISTRIBUTOR_ABI as AbiItem[], process.env.DISTRIBUTOR_NORMAL_ADDRESS)
 export const distributorTest = new l2Client.eth.Contract(DISTRIBUTOR_ABI as AbiItem[], process.env.DISTRIBUTOR_TEST_ADDRESS)
